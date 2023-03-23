0098579ce5a01cbb7048ef0aea1306456ff0b301
Shikharev Rostislav <rshiharev@yandex.ru>
2023-03-23 20:20:43 +0300

    added login form and admin page

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,20 +1,70 @@
-from flask import Flask, render_template
-from webapp.weather import weather_by_city
+from flask import Flask, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 
-from webapp.model import db, News
+from webapp.model import db, News, User
+from webapp.weather import weather_by_city
+from webapp.forms import LoginForm
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = 'login'
+
     @app.route('/')
     def index():
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
         page_title = 'Новости Python'
-
         news_list = News.query.order_by(News.published.desc()).all()
-
         return render_template('index.html', page_title=page_title, weather=weather, news_list=news_list)
 
+
+    @app.route('/login')
+    def login():
+        if current_user.is_authenticated:
+            flash('Вы уже залогинены')
+            return redirect(url_for('index'))
+        title = "Авторизация"
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
+    
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(user_id)
+    
+
+    @app.route('/process-login', methods=['POST'])
+    def process_login():
+        form = LoginForm()
+        if form.validate_on_submit():
+            user = User.query.filter(User.username == form.username.data).first()
+            print('запрос есть')
+            if user and user.check_password(form.password.data):
+                login_user(user)
+                flash('Вы вошли на сайт')
+                return redirect(url_for('index'))
+            
+        flash('Неправильное имя пользователя или пароль')
+        return redirect(url_for('login'))
+    
+
+    @app.route('/logout')
+    def logout():
+        logout_user()
+        flash('Вы успешно разлогинились')
+        return redirect(url_for('index'))
+    
+    
+    @app.route('/admin')
+    @login_required
+    def admin_index():
+        if current_user.is_admin:
+            return 'Привет админ'
+        else:
+            return 'Ты не админ'
+    
     return app
--- webapp/model.py
+++ webapp/model.py
@@ -1,4 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
@@ -9,5 +13,30 @@ class News(db.Model):
     published = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.Text, nullable=True)
 
+
     def __repr__(self):
         return '<News {} {}>'.format(self.title, self.url)
+    
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True, index=True)
+    password = db.Column(db.String(128))
+    role = db.Column(db.String(128), index=True)
+
+
+    def __repr__(self) -> str:
+        return '<User {}>'.format(self.username)
+    
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
